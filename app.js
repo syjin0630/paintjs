94bfe3347de1b1f0176e0b31abd236735ee45af1
syjin0630 <syjjj0630@gmail.com>
2021-12-16 23:25:12 +0900

    선 그리기

---
+++ app.js
@@ -0,0 +1,38 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+function stopPainting() {
+  painting = false;
+}
+function startPainting() {
+  painting = true;
+}
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+function onMouseDown(event) {
+  painting = true;
+}
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  //마우스다운은 클릭
+  canvas.addEventListener("mouseup", stopPainting);
+  //마우스업은 클릭X
+  canvas.addEventListener("mouseleave", stopPainting);
+}
